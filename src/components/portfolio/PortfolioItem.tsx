8cedb0b881f720ee4d8b4a6c5b7327bb7d3789d9
bejaminfkile <benjaminfkile@gmail.com>
2025-07-24 20:29:46 -0500

    portfolio item tweaks and new favicon

--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -18,15 +18,18 @@ const PortfolioItem: FunctionComponent<PortfolioItemProps> = (props) => {
 
     return (
         <div className="portfolio-item">
-                <a className="portfolio-link" data-bs-toggle="modal" href={`#portfolioModal${index}`}>
-                    {/* <div className="portfolio-hover">
+            <a className="portfolio-link" data-bs-toggle="modal" href={`#portfolioModal${index}`}>
+                {/* <div className="portfolio-hover">
                     <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
                 </div> */}
-                    {portfolioItem.media_type < 1 && <img crossOrigin="anonymous" alt="..." src={`${process.env.REACT_APP_API_URL}/api/media?key=images/${portfolioItem.file_name}`} />}
-                    {portfolioItem.media_type > 0 && <video style={{ transform: `scaleY(${portfolioItem.transform_value})` }} id={`video-${index}`} crossOrigin="anonymous" className="profolio-video-player" width="350" muted autoPlay loop>
-                        <source src={`${process.env.REACT_APP_API_URL}/api/media?key=videos/${portfolioItem.file_name}`} type="video/mp4" />
-                    </video>}
-                </a>
+                {portfolioItem.media_type < 1 &&
+                    <div className="portfolio-item-img-wrapper pmc-gradient">
+                        <img crossOrigin="anonymous" alt="..." src={`${process.env.REACT_APP_API_URL}/api/media?key=images/${portfolioItem.file_name}`} />
+                    </div>}
+                {portfolioItem.media_type > 0 && <video style={{ transform: `scaleY(${portfolioItem.transform_value})` }} id={`video-${index}`} crossOrigin="anonymous" className="profolio-video-player" width="350" muted autoPlay loop>
+                    <source src={`${process.env.REACT_APP_API_URL}/api/media?key=videos/${portfolioItem.file_name}`} type="video/mp4" />
+                </video>}
+            </a>
             <div className="portfolio-caption">
                 <div className="portfolio-caption-heading">{portfolioItem.title}</div>
             </div>
--- src/components/portfolio/PortfolioItemModal.tsx
+++ src/components/portfolio/PortfolioItemModal.tsx
@@ -37,7 +37,13 @@ const PortfolioItemModal: FunctionComponent<Props> = (props) => {
                                     {portfolioItem.media_type > 0 && <video id={`modal-video-${index}`} crossOrigin="anonymous" className="profolio-modal-video-player" width="100%" muted autoPlay loop controls>
                                         <source src={`${process.env.REACT_APP_API_URL}/api/media?key=videos/${portfolioItem.file_name}`} type="video/mp4" />
                                     </video>}
-                                    {portfolioItem.media_type < 1 && <img crossOrigin="anonymous" alt="..." src={`${process.env.REACT_APP_API_URL}/api/media?key=images/${portfolioItem.file_name}`} />}
+                                    {portfolioItem.media_type < 1 &&
+                                        <div className="portfolio-modal-content-image-wrapper-wrapper">
+                                            <div className="portfolio-modal-content-image-wrapper pmc-gradient">
+                                                <img crossOrigin="anonymous" alt="..." src={`${process.env.REACT_APP_API_URL}/api/media?key=images/${portfolioItem.file_name}`} />
+                                            </div>
+                                        </div>
+                                    }
                                     <p>{portfolioItem.description}</p>
                                     {portfolioItem.repo && <div className="portfolio-link-item">
                                         <a href={portfolioItem.repo} target="_blank" rel="noreferrer">Check out the repo</a>
